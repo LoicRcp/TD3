648c8ae4ac15a69333c2ffc4bd1ce8381f69d7bc
LoicRcp <loic.recoupe@gmail.com>
2023-11-05 18:45:56 +0100

    added maxCount function

--- comsoc/types.go
+++ comsoc/types.go
@@ -16,3 +16,16 @@ func rank(alt Alternative, prefs []Alternative) int {
 func isPref(alt1, alt2 Alternative, prefs []Alternative) bool {
 	return rank(alt1, prefs) < rank(alt2, prefs)
 }
+
+func maxCount(count Count) (bestAlts []Alternative) {
+	bestScore := 0
+	for alternative, score := range count {
+		if score > bestScore {
+			bestScore = score
+			bestAlts = []Alternative{alternative}
+		} else if score == bestScore {
+			bestAlts = append(bestAlts, alternative)
+		}
+	}
+	return bestAlts
+}
